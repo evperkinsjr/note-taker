c12badd59c49591b15090dae1e1e70ac074027d7
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-07-15 17:21:38 -0400

    added dependencies and api routing

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,41 @@
-// Load data
-
+// Dependencies
+const fs = require('fs');
+const uuid = require('uuid');
 
 // API Routing
+module.exports = (app) => {
+    const notesList = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
 
-// GET Requests
+    // GET Requests
+    app.get('/api/notes', (req, res) => {
+        return res.json(notesList);
+    });
 
+    // POST Requests
+    app.post('/api/notes', (req, res) => {
+        
+        // Stores info
+        const newNote = req.body;
 
-// POST Requests
+        // Sets id property
+        newNote.id = uuid.v4();
 
+        // Adds note
+        notesList.push(newNote);
 
-// DELETE Requests
+        // Returns notes
+        return res.json(notesList);
+    });
+
+    // DELETE Requests
+    app.delete('api/notes/:id', (req, res) => {
+        
+        // find note
+        const deleteNote = notesList.find(note => note.id === req.params.id);
+
+        // delete note
+        notesList.splice(notesList.indexOf(deleteNote), 1);
+
+        res.end('Note deleted');
+    });
+}
