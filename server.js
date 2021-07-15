90bd7232192d668003916348abef965571164463
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-07-15 13:53:14 -0400

    added code based on pseudo-code

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 // Dependencies
+const express = require('express');
 
 // Express Configuration
+const app = express();
 
-// Sets the port
+// Sets the port for listening
+const PORT = process.env.PORT || 3000;
 
 // Sets up the Express App for data parsing
+app.use(express.static(`${__dirname}/public`));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Router
+require('#')(app);
+require('#')(app);
 
 // Starts the server to begin listening
+app.listen(PORT, () => {
+    console.log(`App listening on PORT: ${PORT}`);
+  });
