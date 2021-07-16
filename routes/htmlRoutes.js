ceec388c94a18d9ff56d47bdfa247af7a0c9a261
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-07-15 23:40:27 -0400

    minor change

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,7 +4,7 @@ const htmlRoute = express.Router();
 const path = require('path');
 
 // Home page route
-htmlRoute.get('*', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
+htmlRoute.get('/', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
 
 // Notes page route
 htmlRoute.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '../public/notes.html')));
